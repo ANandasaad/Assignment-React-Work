2e9444d8fed1a7eabcdcf4cc0785673616b76203
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-04-02 16:47:29 +0530

    commit

--- src/compontents/RestaurantCard.js
+++ src/compontents/RestaurantCard.js
@@ -10,8 +10,8 @@ const RestaurantCard = ({
 
   }) => {
     return (
-      <div className="w-54 h-52 my-8 box-border" >
-        <div className="w-[250px] p-[5px]  shadow-xl mx-[15px] my-[5px] bg-pink-50 hover:bg-pink-100 ">
+      <div className=" h-60 my-10 box-border" >
+        <div className="w-[250px] h-[280px] p-[5px]  shadow-xl mx-[15px] my-[5px] bg-pink-50 hover:bg-pink-100 ">
         <img src={IMG_CDN_URL + cloudinaryImageId} />
         <h2 className="font-bold">{name}</h2>
         <h3>{cuisines.join(" , ")}</h3>
